cccbb83725593465f3cce917eb8dff105580a2f0
rokutvaccount <seenuchauhann+1@outlook.com>
2026-08-03 02:57:30 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,20 @@
+project = "Roku TV Login"
+author = "Roku TV Login"
+release = '1.0'
 
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
